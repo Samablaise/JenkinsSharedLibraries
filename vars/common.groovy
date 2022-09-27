bdba17b56ac331e2443f2b7cbab735ec4abec0d8
Samablaise <111323851+Samablaise@users.noreply.github.com>
2022-09-27 22:41:16 +0100

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,1 +1,15 @@
-
+def call(String stageName){
+  
+  if ("${stageName}" == "Build")
+     {
+       sh "mvn clean package"
+     }
+  else if ("${stageName}" == "Sonarqube Report")
+     {
+       sh "mvn clean sonar:sonar"
+     }
+   else if ("${stageName}" == "Upload to Nexus")
+     {
+       sh "mvn clean deploy"
+     }      
+}
